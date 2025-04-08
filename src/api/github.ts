c1143c7e0b5ac261c2e17c9f4553ece998db06bb
xandebnd <xandebnd@hotmail.com>
2025-04-08 19:56:55 -0300

    fix-api

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,7 +1,7 @@
+import { LRUCache } from "lru-cache";
 import type { NextApiRequest, NextApiResponse } from "next";
-import LRU from "lru-cache";
 
-const rateLimit = new LRU({
+const rateLimit = new LRUCache({
 	max: 100, // número máximo de IPs a serem armazenados
 	ttl: 1000 * 60, // tempo de vida em cache (1 minuto)
 });
@@ -18,16 +18,13 @@ export default async function handler(
 
 	const requests = rateLimit.get(ip as string) || 0;
 
-	if (requests >= 10) {
-		return res
-			.status(429)
-			.json({
-				message:
-					"Limite de requisições atingido. Tente novamente em instantes.",
-			});
+	if ((requests as number) >= 10) {
+		return res.status(429).json({
+			message: "Limite de requisições atingido. Tente novamente em instantes.",
+		});
 	}
 
-	rateLimit.set(ip as string, requests + 1);
+	rateLimit.set(ip as string, (requests as number) + 1);
 
 	const { username, type = "user" } = req.query;
 
@@ -51,7 +48,7 @@ export default async function handler(
 		}
 
 		return res.status(200).json(data);
-	} catch (error) {
+	} catch {
 		return res.status(500).json({ message: "Erro interno do servidor." });
 	}
 }
